516964adf944cc0cb698619274c816abbec7d78c
dreamins <dreamins@gmail.com>
2012-04-04 00:39:46 -0700

    Fixed Route53 calls. Complied to Apache License 2.0, added license and readme.

--- Route53DDNS/Runner.cs
+++ Route53DDNS/Runner.cs
@@ -38,15 +38,30 @@ namespace Route53DDNS
 
         private void doIt()
         {
-            logger.Info("Retrieving IP.");
-            string myIP = IPRetreiver.getIP(opts);
-            logger.Info("Ip is" + myIP);
+            try 
+            {
+                logger.Info("Retrieving IP.");
+                string myIP = IPRetreiver.getIP(opts).Trim();
+                logger.Info("Got IP [" + myIP + "]");
 
-            Route53Client r53Client = new Route53Client(opts.AWSAccessKey, opts.AWSSecretKey);
-            Route53AIPForHostedZoneAccessor accessor = new Route53AIPForHostedZoneAccessor(r53Client, opts.HostedZoneId);
+                logger.Info("Retrieving IP from Route53");
+                Route53Client client = new Route53Client(opts.AWSAccessKey, opts.AWSSecretKey);
+                Route53AIPForHostedZoneAccessor accessor = new Route53AIPForHostedZoneAccessor(client, opts.HostedZoneId);
+                string oldIP = accessor.get().Trim();
 
-            string oldIP = accessor.get();
-            logger.Info("Old ip is" + myIP);
+                logger.Info("DNS is pointing to " + oldIP);
+
+                if (oldIP != null && String.Equals(oldIP, myIP))
+                {
+                    logger.Info("Nothing changed. Bye monster.");
+                    return;
+                }
+
+                logger.Info("Updating record at Route53 ");
+                new Route53UpdateARecordForHostedZoneAccessor(client, opts.HostedZoneId, oldIP).get();
+            } catch (exception.Route53DDNSException ex ) {
+                logger.Error("Got an exception haven't done anything perhaps." + ex.Message);
+            }
         }
 
         private void updateR53(Options opts)
---
+++ Route53DDNS/accessor/Route53UpdateARecordForHostedZoneAccessor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Amazon.Route53;
+using Amazon.Route53.Model;
+
+using Route53DDNS.client;
+using Route53DDNS.type;
+using Route53DDNS.exception;
+
+namespace Route53DDNS.accessor
+{
+    // Updates hosted zone's A record with a pre-defined IP
+    // note there is no update in route 53 instead it has delete and create in same batch
+    // so actually the accessor shall perform two requests - get RR and delete/create it
+    class Route53UpdateARecordForHostedZoneAccessor: Accessor<bool>
+    {
+        private Route53Client client;
+        private string hostedZone;
+        private string dnsIP;
+
+        public Route53UpdateARecordForHostedZoneAccessor(Route53Client client, string hostedZone, string dnsIP)
+        {
+            this.client = client;
+            this.hostedZone = hostedZone;
+            this.dnsIP = dnsIP;
+        }
+
+        public override bool get()
+        {
+            try
+            {
+                ListResourceRecordSetsResponse response;
+                String nextRecordIdentifier = null;
+                ResourceRecord recordToUpdate = null;
+                do
+                {
+                    response = client.listRecordSets(hostedZone, nextRecordIdentifier);
+                    nextRecordIdentifier = response.ListResourceRecordSetsResult.NextRecordIdentifier;
+
+                    foreach (ResourceRecordSet rrset in response.ListResourceRecordSetsResult.ResourceRecordSets)
+                    {
+                        if (rrset.Type != "A")
+                        {
+                            continue;
+                        }
+                        // well we still assume that there is only one A record
+                        // but at least make minimal defence do not update it
+                        // if ip doesn't match
+
+                        recordToUpdate = rrset.ResourceRecords.Find(delegate (ResourceRecord rr) {
+                            return !String.IsNullOrWhiteSpace(rr.Value) && rr.Value.Equals(dnsIP);
+                        });
+
+                        //update!
+                        return true;
+                    }
+
+                } while (response.ListResourceRecordSetsResult.IsTruncated);
+
+                throw new ConfigurationException("Cannot get previous IP from Route53. Make sure you have one A record in your hosted zone!");
+            }
+            catch (InvalidInputException ex)
+            {
+                throw new ConfigurationException("Cannot call AWS Route53 " + ex.Message);
+            }
+        }
+    }
+}
