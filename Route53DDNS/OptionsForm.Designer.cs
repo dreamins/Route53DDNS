71dc7abbbdfdb5485fed232477bdcc9b5eed18de
dreamins <dreamins@gmail.com>
2012-05-02 21:13:52 -0700

    Tabbed option form. Removed unnecessary leftover file.

--- Route53DDNS/OptionsForm.Designer.cs
+++ Route53DDNS/OptionsForm.Designer.cs
@@ -40,12 +40,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.hostedZoneId = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.tabGeneralPage = new System.Windows.Forms.TabPage();
+            this.tabAWSPage = new System.Windows.Forms.TabPage();
+            this.tabAdvancedPage = new System.Windows.Forms.TabPage();
+            this.TabControl.SuspendLayout();
+            this.tabGeneralPage.SuspendLayout();
+            this.tabAWSPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // externalIPNeeded
             // 
             this.externalIPNeeded.AutoSize = true;
-            this.externalIPNeeded.Location = new System.Drawing.Point(12, 12);
+            this.externalIPNeeded.Location = new System.Drawing.Point(32, 23);
             this.externalIPNeeded.Name = "externalIPNeeded";
             this.externalIPNeeded.Size = new System.Drawing.Size(182, 17);
             this.externalIPNeeded.TabIndex = 0;
@@ -54,7 +61,7 @@
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(193, 206);
+            this.saveButton.Location = new System.Drawing.Point(91, 206);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 1;
@@ -64,7 +71,7 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(313, 206);
+            this.cancelButton.Location = new System.Drawing.Point(186, 206);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 2;
@@ -75,7 +82,7 @@
             // hasInitialDelay
             // 
             this.hasInitialDelay.AutoSize = true;
-            this.hasInitialDelay.Location = new System.Drawing.Point(12, 35);
+            this.hasInitialDelay.Location = new System.Drawing.Point(32, 54);
             this.hasInitialDelay.Name = "hasInitialDelay";
             this.hasInitialDelay.Size = new System.Drawing.Size(232, 17);
             this.hasInitialDelay.TabIndex = 3;
@@ -84,15 +91,15 @@
             // 
             // timerPeriodSec
             // 
-            this.timerPeriodSec.Location = new System.Drawing.Point(12, 57);
+            this.timerPeriodSec.Location = new System.Drawing.Point(32, 88);
             this.timerPeriodSec.Name = "timerPeriodSec";
-            this.timerPeriodSec.Size = new System.Drawing.Size(26, 20);
+            this.timerPeriodSec.Size = new System.Drawing.Size(34, 20);
             this.timerPeriodSec.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 60);
+            this.label1.Location = new System.Drawing.Point(72, 91);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(173, 13);
             this.label1.TabIndex = 5;
@@ -100,15 +107,15 @@
             // 
             // awsAccessKey
             // 
-            this.awsAccessKey.Location = new System.Drawing.Point(313, 12);
+            this.awsAccessKey.Location = new System.Drawing.Point(6, 20);
             this.awsAccessKey.Name = "awsAccessKey";
-            this.awsAccessKey.Size = new System.Drawing.Size(173, 20);
+            this.awsAccessKey.Size = new System.Drawing.Size(171, 20);
             this.awsAccessKey.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(492, 12);
+            this.label2.Location = new System.Drawing.Point(185, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 7;
@@ -116,15 +123,15 @@
             // 
             // awsSecretKey
             // 
-            this.awsSecretKey.Location = new System.Drawing.Point(313, 35);
+            this.awsSecretKey.Location = new System.Drawing.Point(6, 51);
             this.awsSecretKey.Name = "awsSecretKey";
-            this.awsSecretKey.Size = new System.Drawing.Size(173, 20);
+            this.awsSecretKey.Size = new System.Drawing.Size(171, 20);
             this.awsSecretKey.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(492, 35);
+            this.label3.Location = new System.Drawing.Point(185, 54);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(84, 13);
             this.label3.TabIndex = 9;
@@ -132,42 +139,88 @@
             // 
             // hostedZoneId
             // 
-            this.hostedZoneId.Location = new System.Drawing.Point(313, 58);
+            this.hostedZoneId.Location = new System.Drawing.Point(6, 82);
             this.hostedZoneId.Name = "hostedZoneId";
-            this.hostedZoneId.Size = new System.Drawing.Size(173, 20);
+            this.hostedZoneId.Size = new System.Drawing.Size(171, 20);
             this.hostedZoneId.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(492, 58);
+            this.label4.Location = new System.Drawing.Point(185, 85);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(105, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "AWS HostedZoneID";
             // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.tabGeneralPage);
+            this.TabControl.Controls.Add(this.tabAWSPage);
+            this.TabControl.Controls.Add(this.tabAdvancedPage);
+            this.TabControl.Location = new System.Drawing.Point(12, 12);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(303, 178);
+            this.TabControl.TabIndex = 12;
+            // 
+            // tabGeneralPage
+            // 
+            this.tabGeneralPage.Controls.Add(this.externalIPNeeded);
+            this.tabGeneralPage.Controls.Add(this.label1);
+            this.tabGeneralPage.Controls.Add(this.timerPeriodSec);
+            this.tabGeneralPage.Controls.Add(this.hasInitialDelay);
+            this.tabGeneralPage.Location = new System.Drawing.Point(4, 22);
+            this.tabGeneralPage.Name = "tabGeneralPage";
+            this.tabGeneralPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGeneralPage.Size = new System.Drawing.Size(295, 152);
+            this.tabGeneralPage.TabIndex = 0;
+            this.tabGeneralPage.Text = "General";
+            this.tabGeneralPage.UseVisualStyleBackColor = true;
+            // 
+            // tabAWSPage
+            // 
+            this.tabAWSPage.Controls.Add(this.awsAccessKey);
+            this.tabAWSPage.Controls.Add(this.hostedZoneId);
+            this.tabAWSPage.Controls.Add(this.label4);
+            this.tabAWSPage.Controls.Add(this.awsSecretKey);
+            this.tabAWSPage.Controls.Add(this.label2);
+            this.tabAWSPage.Controls.Add(this.label3);
+            this.tabAWSPage.Location = new System.Drawing.Point(4, 22);
+            this.tabAWSPage.Name = "tabAWSPage";
+            this.tabAWSPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAWSPage.Size = new System.Drawing.Size(295, 152);
+            this.tabAWSPage.TabIndex = 1;
+            this.tabAWSPage.Text = "AWS";
+            this.tabAWSPage.UseVisualStyleBackColor = true;
+            // 
+            // tabAdvancedPage
+            // 
+            this.tabAdvancedPage.Location = new System.Drawing.Point(4, 22);
+            this.tabAdvancedPage.Name = "tabAdvancedPage";
+            this.tabAdvancedPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAdvancedPage.Size = new System.Drawing.Size(295, 152);
+            this.tabAdvancedPage.TabIndex = 2;
+            this.tabAdvancedPage.Text = "Advanced";
+            this.tabAdvancedPage.UseVisualStyleBackColor = true;
+            // 
             // OptionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 241);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.hostedZoneId);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.awsSecretKey);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.awsAccessKey);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.timerPeriodSec);
-            this.Controls.Add(this.hasInitialDelay);
+            this.ClientSize = new System.Drawing.Size(355, 256);
+            this.Controls.Add(this.TabControl);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.externalIPNeeded);
             this.Name = "OptionsForm";
             this.Text = "Options";
             this.Load += new System.EventHandler(this.OptionsForm_Load);
+            this.TabControl.ResumeLayout(false);
+            this.tabGeneralPage.ResumeLayout(false);
+            this.tabGeneralPage.PerformLayout();
+            this.tabAWSPage.ResumeLayout(false);
+            this.tabAWSPage.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -185,5 +238,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox hostedZoneId;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage tabGeneralPage;
+        private System.Windows.Forms.TabPage tabAWSPage;
+        private System.Windows.Forms.TabPage tabAdvancedPage;
     }
 }
